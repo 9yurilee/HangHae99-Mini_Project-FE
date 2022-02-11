5ee08906a3859c20dd50452163878c8f1081b772
rilee <97436315+degurrrrrr@users.noreply.github.com>
2022-02-11 22:13:58 +0900

    Delete src directory

--- src/pages/Edit.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Edit = () => {
-  return (
-    <h3>여긴 임시수정페이지야~~</h3>
-  )
-}
-
-export default Edit;
--- src/pages/index.js
+++
@@ -1,8 +0,0 @@
-import Detail from './Detail'
-import Login from './Login'
-import Main from './Main'
-import PostWrite from './PostWrite'
-import SignUp from './SignUp'
-import Edit from './Edit'
-
-export {Detail, Login, Main, PostWrite, SignUp, Edit};
