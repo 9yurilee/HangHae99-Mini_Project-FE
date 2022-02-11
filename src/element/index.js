7857a91d2737cac6277487fdd9a1274049169a71
rilee <97436315+degurrrrrr@users.noreply.github.com>
2022-02-11 21:50:27 +0900

    Delete src directory

--- src/element/index.js
+++
@@ -1,7 +0,0 @@
-import Button from './Button';
-import Grid from './Grid';
-import Image from './Image';
-import Input from './Input';
-import Text from './Text';
-
-export {Button, Grid, Image, Input, Text};
